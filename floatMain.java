b3b0571f7c340ddac0677b14a796db0c17eb425f
Лавренов Евгений <lavr020775@gmail.com>
2023-04-24 13:58:51 +0300

    prog1

---
+++ floatMain.java
@@ -0,0 +1,71 @@
+// Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения, вместо этого, необходимо повторно запросить у пользователя ввод данных.
+// 2. Если необходимо, исправьте данный код (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
+// Дан следующий код, исправьте его там, где требуется (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
+// Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку. Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class floatMain {
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+    public static void main(String[] args) {
+        inputUser();
+        zadanie2();
+        zadanie3();5
+    }
+
+    static float inputUser(){
+        while (true){
+            System.out.print("Введите число с плавающей точкой: ");
+            if (scanner.hasNextFloat()){
+                float floatNumber = scanner.nextFloat();
+                return floatNumber;
+            }
+            else{
+                System.out.println("Число для поиска указано некорректно.\nПовторите попытку ввода.");
+                scanner.nextLine();
+            }
+        }
+
+    }
+
+        //2. Если необходимо, исправьте данный код
+        //  (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+        static void zadanie2(){
+            int[] intArray = new int[random.nextInt(2) + 9];
+            for (int i = 0; i < intArray.length; i++) {
+                intArray[i] = random.nextInt(66);
+            }
+            try {
+                int d = random.nextInt(2);
+                double catchedRes1 = intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            } catch (ArithmeticException e) {
+                System.out.println("Catching exception: " + e);
+            }
+
+        }
+
+
+            // Дан следующий код, исправьте его там, где требуется 
+        // (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
+        static void zadanie3(){
+            int[] intArray = new int[random.nextInt(2) + 9];
+            for (int i = 0; i < intArray.length; i++) {
+                intArray[i] = random.nextInt(66);
+            }
+            try {
+                int d = random.nextInt(2);
+                double catchedRes1 = intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            } catch (ArithmeticException e) {
+                System.out.println("Catching exception: " + e);
+            }
+        }
+}
